d7fb6891c6c5174e10d43e58d6509323d4248a73
Lucia Cordero <escorderolucy@gmail.com>
2025-12-09 13:05:44 +0100

    functional predict endpoint awaiting real model

---
+++ reefsight/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+
+def predict_image():
+    """
+    Make a bleaching prediction using the latest trained computer vision model
+    """
+
+    print("\n⭐️ Use case: predict images")
+
+    pass #CODE WILL GO HERE
+
+
+
+
+def predict_tabular(X_pred: pd.DataFrame = None) -> np.ndarray:
+    """
+    Make a bleaching prediction using the latest trained tabular model
+    """
+
+    print("\n⭐️ Use case: predict tabular")
+
+    pass #CODE WILL GO HERE
